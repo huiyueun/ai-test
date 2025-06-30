6f5b493e57f0bfa36887c87f9abb5c06654b2c5a
huiyu <35286162+huiyueun@users.noreply.github.com>
2025-06-30 17:02:33 +0900

    Update Step1.py

--- Step1.py
+++ Step1.py
@@ -1,87 +1,15 @@
-(unsloth_env) huiyu@huiyu-linux2:~/Workspace/test-1$ python3 step1.py 
-🦥 Unsloth: Will patch your computer to enable 2x faster free finetuning.
-🦥 Unsloth Zoo will now patch everything to make training faster!
-Traceback (most recent call last):
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 700, in urlopen
-    httplib_response = self._make_request(
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 383, in _make_request
-    self._validate_conn(conn)
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 1017, in _validate_conn
-    conn.connect()
-  File "/usr/lib/python3/dist-packages/urllib3/connection.py", line 411, in connect
-    self.sock = ssl_wrap_socket(
-  File "/usr/lib/python3/dist-packages/urllib3/util/ssl_.py", line 449, in ssl_wrap_socket
-    ssl_sock = _ssl_wrap_socket_impl(
-  File "/usr/lib/python3/dist-packages/urllib3/util/ssl_.py", line 493, in _ssl_wrap_socket_impl
-    return ssl_context.wrap_socket(sock, server_hostname=server_hostname)
-  File "/usr/lib/python3.10/ssl.py", line 513, in wrap_socket
-    return self.sslsocket_class._create(
-  File "/usr/lib/python3.10/ssl.py", line 1100, in _create
-    self.do_handshake()
-  File "/usr/lib/python3.10/ssl.py", line 1371, in do_handshake
-    self._sslobj.do_handshake()
-ConnectionResetError: [Errno 104] Connection reset by peer
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/home/huiyu/.local/lib/python3.10/site-packages/requests/adapters.py", line 667, in send
-    resp = conn.urlopen(
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 756, in urlopen
-    retries = retries.increment(
-  File "/usr/lib/python3/dist-packages/urllib3/util/retry.py", line 534, in increment
-    raise six.reraise(type(error), error, _stacktrace)
-  File "/usr/lib/python3/dist-packages/six.py", line 718, in reraise
-    raise value.with_traceback(tb)
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 700, in urlopen
-    httplib_response = self._make_request(
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 383, in _make_request
-    self._validate_conn(conn)
-  File "/usr/lib/python3/dist-packages/urllib3/connectionpool.py", line 1017, in _validate_conn
-    conn.connect()
-  File "/usr/lib/python3/dist-packages/urllib3/connection.py", line 411, in connect
-    self.sock = ssl_wrap_socket(
-  File "/usr/lib/python3/dist-packages/urllib3/util/ssl_.py", line 449, in ssl_wrap_socket
-    ssl_sock = _ssl_wrap_socket_impl(
-  File "/usr/lib/python3/dist-packages/urllib3/util/ssl_.py", line 493, in _ssl_wrap_socket_impl
-    return ssl_context.wrap_socket(sock, server_hostname=server_hostname)
-  File "/usr/lib/python3.10/ssl.py", line 513, in wrap_socket
-    return self.sslsocket_class._create(
-  File "/usr/lib/python3.10/ssl.py", line 1100, in _create
-    self.do_handshake()
-  File "/usr/lib/python3.10/ssl.py", line 1371, in do_handshake
-    self._sslobj.do_handshake()
-urllib3.exceptions.ProtocolError: ('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/home/huiyu/Workspace/test-1/step1.py", line 5, in <module>
-    model, tokenizer = FastModel.from_pretrained(
-  File "/home/huiyu/.local/lib/python3.10/site-packages/unsloth/models/loader.py", line 614, in from_pretrained
-    files = HfFileSystem(token = token).glob(f"{model_name}/*.json")
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/hf_file_system.py", line 520, in glob
-    path = self.resolve_path(path, revision=kwargs.get("revision")).unresolve()
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/hf_file_system.py", line 209, in resolve_path
-    repo_and_revision_exist, err = self._repo_and_revision_exist(repo_type, repo_id, revision)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/hf_file_system.py", line 125, in _repo_and_revision_exist
-    self._api.repo_info(
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/utils/_validators.py", line 114, in _inner_fn
-    return fn(*args, **kwargs)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/hf_api.py", line 2844, in repo_info
-    return method(
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/utils/_validators.py", line 114, in _inner_fn
-    return fn(*args, **kwargs)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/hf_api.py", line 2628, in model_info
-    r = get_session().get(path, headers=headers, timeout=timeout, params=params)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/requests/sessions.py", line 602, in get
-    return self.request("GET", url, **kwargs)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/requests/sessions.py", line 589, in request
-    resp = self.send(prep, **send_kwargs)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/requests/sessions.py", line 703, in send
-    r = adapter.send(request, **kwargs)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/huggingface_hub/utils/_http.py", line 96, in send
-    return super().send(request, *args, **kwargs)
-  File "/home/huiyu/.local/lib/python3.10/site-packages/requests/adapters.py", line 682, in send
-    raise ConnectionError(err, request=request)
-requests.exceptions.ConnectionError: (ProtocolError('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer')), '(Request ID: f1b33842-909e-48a5-afae-19e6802e9492)')
+(unsloth_env) huiyu@huiyu-linux2:~/Workspace/test-1$ pip install huggingface_hub
+Defaulting to user installation because normal site-packages is not writeable
+Requirement already satisfied: huggingface_hub in /home/huiyu/.local/lib/python3.10/site-packages (0.33.1)
+Requirement already satisfied: filelock in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (3.18.0)
+Requirement already satisfied: packaging>=20.9 in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (25.0)
+Requirement already satisfied: hf-xet<2.0.0,>=1.1.2 in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (1.1.5)
+Requirement already satisfied: typing-extensions>=3.7.4.3 in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (4.14.0)
+Requirement already satisfied: tqdm>=4.42.1 in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (4.67.1)
+Requirement already satisfied: requests in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (2.32.4)
+Requirement already satisfied: fsspec>=2023.5.0 in /home/huiyu/.local/lib/python3.10/site-packages (from huggingface_hub) (2025.3.0)
+Requirement already satisfied: pyyaml>=5.1 in /usr/lib/python3/dist-packages (from huggingface_hub) (5.4.1)
+Requirement already satisfied: idna<4,>=2.5 in /usr/lib/python3/dist-packages (from requests->huggingface_hub) (3.3)
+Requirement already satisfied: urllib3<3,>=1.21.1 in /usr/lib/python3/dist-packages (from requests->huggingface_hub) (1.26.5)
+Requirement already satisfied: certifi>=2017.4.17 in /usr/lib/python3/dist-packages (from requests->huggingface_hub) (2020.6.20)
+Requirement already satisfied: charset_normalizer<4,>=2 in /home/huiyu/.local/lib/python3.10/site-packages (from requests->huggingface_hub) (3.4.2)
